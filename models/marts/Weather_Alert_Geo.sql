9251a01ced0b5f2e0c2509208e76eaab329ac303
Chris Rogers <w.rogers9999@gmail.com>
2026-04-04 04:42:47 +0000

    Adding first mart

---
+++ models/marts/Weather_Alert_Geo.sql
@@ -0,0 +1,21 @@
+--with row_count as (
+select 
+geo.geo_id_state,
+geo.geo_id_county,
+geo.geoname_county,
+geo.geoname_state,
+count(case when event_severity = 'Moderate' then  nws_alert_id end) as Moderate_Events,
+count(case when event_severity = 'Severe' then  nws_alert_id end) as Severe_Events,
+count(case when event_severity = 'Extreme' then  nws_alert_id end) as Extreme_Events
+from int_geography as geo 
+    inner join int_weatheralerts as WA on 
+        geo.geo_id_county = WA.COUNTY_GEO_ID
+
+where alert_status <> 'Test'
+group by 
+geo.geo_id_state,
+geo.geo_id_county,
+geo.geoname_county,
+geo.geoname_state
+
+
