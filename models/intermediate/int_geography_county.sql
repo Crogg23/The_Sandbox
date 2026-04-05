9ca3b34cb21903f3c629d40cc8a539e6154606ff
Chris Rogers <w.rogers9999@gmail.com>
2026-04-05 04:10:32 +0000

    again

--- models/intermediate/int_geography_county.sql
+++ models/intermediate/int_geography_county.sql
@@ -1,43 +1,27 @@
 {{ config(materialized='table') }}
 
-SELECT 
-Continent.GEO_ID as Geo_ID_Continent, 
-Country.GEO_ID as Geo_ID_Country,
-State.GEO_ID as Geo_ID_State,
-County.GEO_ID as Geo_ID_County,
-CensusTract.GEO_ID as Geo_ID_CensusTract,
-Continent.GEO_NAME as GeoName_Continent,
-Country.Geo_Name as GeoName_Country,
-State.Geo_Name as GeoName_State,
-County.Geo_Name as GeoName_County,
-CensusTract.GEO_NAME as GeoName_CensusTract,
-MAX(case when GC.relationship_type = 'coordinates_wkt' then gc.value end) as WKT_Coordinates,
-MAX(case when GC.relationship_type = 'coordinates_geojson' then gc.value end) as JSON_Coordinates
-
-FROM  {{ ref('stg_geography_index') }} as Continent
-    inner join {{ ref('stg_geography_relationships') }} as Country on 
-        Continent.GEO_ID = Country.RELATED_GEO_ID
-        and Country.level = 'Country'
-    inner join {{ ref('stg_geography_relationships') }} as State on 
-        Country.GEO_ID = State.RELATED_GEO_ID
-        and State.level = 'State'
-     inner join {{ ref('stg_geography_relationships') }} as County on 
-        State.GEO_ID = County.RELATED_GEO_ID
-        and County.level = 'County'
-    inner join {{ ref('stg_geography_relationships') }} as CensusTract on 
-        County.GEO_ID = CensusTract.RELATED_GEO_ID
-        and CensusTract.level = 'CensusTract'
-    inner join {{ ref('stg_geography_characteristics') }} as GC on
-        County.Geo_id = GC.GEO_ID
-where Continent.level = 'Continent'
-
-group by Continent.GEO_ID, 
-Country.GEO_ID,
-State.GEO_ID,
-County.GEO_ID,
-CensusTract.GEO_ID,
-Continent.GEO_NAME,
-Country.Geo_Name,
-State.Geo_Name,
-County.Geo_Name,
-CensusTract.GEO_NAME
+select 
+    continent.geo_id as geo_id_continent, 
+    country.geo_id as geo_id_country,
+    state.geo_id as geo_id_state,
+    county.geo_id as geo_id_county,
+    continent.geo_name as geoname_continent,
+    country.geo_name as geoname_country,
+    state.geo_name as geoname_state,
+    county.geo_name as geoname_county,
+    max(case when gc.relationship_type = 'coordinates_wkt' then gc.value end) as wkt_coordinates,
+    max(case when gc.relationship_type = 'coordinates_geojson' then gc.value end) as json_coordinates
+from {{ ref('stg_geography_index') }} as continent
+    inner join {{ ref('stg_geography_relationships') }} as country on 
+        continent.geo_id = country.related_geo_id
+        and country.level = 'Country'
+    inner join {{ ref('stg_geography_relationships') }} as state on 
+        country.geo_id = state.related_geo_id
+        and state.level = 'State'
+    inner join {{ ref('stg_geography_relationships') }} as county on 
+        state.geo_id = county.related_geo_id
+        and county.level = 'County'
+    inner join {{ ref('stg_geography_characteristics') }} as gc on
+        county.geo_id = gc.geo_id
+where continent.level = 'Continent'
+group by 1,2,3,4,5,6,7,8
