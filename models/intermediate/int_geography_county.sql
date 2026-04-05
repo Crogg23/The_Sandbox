7ea6fab64aed260a90eca4378c2b1d1d5215195f
Rip <w.rogers9999@gmail.com>
2026-04-05 09:55:54 -0700

    Update int_geography_county.sql

--- models/intermediate/int_geography_county.sql
+++ models/intermediate/int_geography_county.sql
@@ -1,42 +1,51 @@
 {{ config(materialized='table') }}
 
 select
-    continent.geo_id     as geo_id_continent,
-    country.geo_id       as geo_id_country,
-    state.geo_id         as geo_id_state,
-    county.geo_id        as geo_id_county,
-    continent.geo_name   as geoname_continent,
-    country.geo_name     as geoname_country,
-    state.geo_name       as geoname_state,
-    county.geo_name      as geoname_county,
+    continent.geo_id          as geo_id_continent,
+    country_rel.geo_id        as geo_id_country,
+    state_rel.geo_id          as geo_id_state,
+    county_rel.geo_id         as geo_id_county,
+    continent.geo_name        as geoname_continent,
+    country_idx.geo_name      as geoname_country,
+    state_idx.geo_name        as geoname_state,
+    county_idx.geo_name       as geoname_county,
     max(case when gc.relationship_type = 'coordinates_wkt' then gc.value end)     as wkt_coordinates,
     max(case when gc.relationship_type = 'coordinates_geojson' then gc.value end) as json_coordinates
 
 from {{ ref('stg_geography_index') }} as continent
 
-inner join {{ ref('stg_geography_relationships') }} as country
-    on continent.geo_id = country.related_geo_id
-    and country.level = 'Country'
+inner join {{ ref('stg_geography_relationships') }} as country_rel
+    on continent.geo_id = country_rel.related_geo_id
+    and country_rel.level = 'Country'
 
-inner join {{ ref('stg_geography_relationships') }} as state
-    on country.geo_id = state.related_geo_id
-    and state.level = 'State'
+inner join {{ ref('stg_geography_index') }} as country_idx
+    on country_rel.geo_id = country_idx.geo_id
 
-inner join {{ ref('stg_geography_relationships') }} as county
-    on state.geo_id = county.related_geo_id
-    and county.level = 'County'
+inner join {{ ref('stg_geography_relationships') }} as state_rel
+    on country_rel.geo_id = state_rel.related_geo_id
+    and state_rel.level = 'State'
+
+inner join {{ ref('stg_geography_index') }} as state_idx
+    on state_rel.geo_id = state_idx.geo_id
+
+inner join {{ ref('stg_geography_relationships') }} as county_rel
+    on state_rel.geo_id = county_rel.related_geo_id
+    and county_rel.level = 'County'
+
+inner join {{ ref('stg_geography_index') }} as county_idx
+    on county_rel.geo_id = county_idx.geo_id
 
 inner join {{ ref('stg_geography_characteristics') }} as gc
-    on county.geo_id = gc.geo_id
+    on county_rel.geo_id = gc.geo_id
 
 where continent.level = 'Continent'
 
 group by
     continent.geo_id,
-    country.geo_id,
-    state.geo_id,
-    county.geo_id,
+    country_rel.geo_id,
+    state_rel.geo_id,
+    county_rel.geo_id,
     continent.geo_name,
-    country.geo_name,
-    state.geo_name,
-    county.geo_name
+    country_idx.geo_name,
+    state_idx.geo_name,
+    county_idx.geo_name
