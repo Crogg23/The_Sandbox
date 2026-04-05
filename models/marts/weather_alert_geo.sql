de179887dbf380599ed79bd0e1740a4668d6ed1f
Chris Rogers <w.rogers9999@gmail.com>
2026-04-05 03:41:58 +0000

    fixing count statements

--- models/marts/weather_alert_geo.sql
+++ models/marts/weather_alert_geo.sql
@@ -6,9 +6,9 @@ geo.geoname_state,
 -- removed: geo.geoname_censustract
 geo.wkt_coordinates,
 geo.json_coordinates,
-count(case when event_severity = 'Moderate' then nws_alert_id end) as moderate_events,
-count(case when event_severity = 'Severe' then nws_alert_id end) as severe_events,
-count(case when event_severity = 'Extreme' then nws_alert_id end) as extreme_events
+count(distinct case when event_severity = 'Moderate' then nws_alert_id end) as moderate_events,
+count(distinct case when event_severity = 'Severe' then nws_alert_id end) as severe_events,
+count(distinct case when event_severity = 'Extreme' then nws_alert_id end) as extreme_events
 from {{ ref('int_geography') }} as geo 
     inner join {{ ref('int_weatheralerts') }} as wa on 
         geo.geo_id_county = wa.county_geo_id
