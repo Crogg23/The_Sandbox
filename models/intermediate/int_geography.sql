06d2690d25211c40be93fa543a4e1ce0f81a4324
Chris Rogers <w.rogers9999@gmail.com>
2026-04-08 15:20:57 +0000

    Adding geo and nws alert ints

--- models/intermediate/int_geography.sql
+++ models/intermediate/int_geography.sql
@@ -8,18 +8,18 @@ CensusTract.GEO_NAME as CensusTract_GeoName,
 County.GEO_NAME as County_GeoName,
 State.GEO_NAME as State_GeoName,
 Country.GEO_NAME as Country_GeoName,
-Continent.GEO_NAME as Continent_GeoName,
- FROM {{ source('snowflake_public_data', 'GEOGRAPHY_INDEX') }} as CensusTract
-    inner join {{source('snowflake_public_data', 'GEOGRAPHY_RELATIONSHIPS') }} as County on 
+Continent.GEO_NAME as Continent_GeoName
+FROM {{ref('stg_geography_index') }} as CensusTract
+    inner join {{ref('stg_geographic_relationships') }} as County on 
         CensusTract.GEO_ID = County.RELATED_GEO_ID
         and County.level = 'County'
-    inner join {{source('snowflake_public_data', 'GEOGRAPHY_RELATIONSHIPS') }} as State on 
+    inner join {{ref('stg_geographic_relationships') }} as State on 
         County.GEO_ID = State.RELATED_GEO_ID
         and State.LEVEL = 'State'
-     inner join {{source('snowflake_public_data', 'GEOGRAPHY_RELATIONSHIPS') }} as Country on 
+     inner join {{ref('stg_geographic_relationships') }} as Country on 
             State.GEO_ID = Country.RELATED_GEO_ID
             and Country.LEVEL = 'Country'
-    inner join {{source('snowflake_public_data', 'GEOGRAPHY_RELATIONSHIPS') }} as Continent on 
+    inner join {{ref('stg_geographic_relationships') }} as Continent on 
             Country.GEO_ID = Continent.RELATED_GEO_ID
             and Continent.LEVEL = 'Continent'
     where CensusTract.level = 'CensusTract'
